06c6f011e51e6c61ebbfde716538e094fb2c3d06
Levente Botos <botos.levente2007@gmail.com>
2023-07-02 11:10:55 +0300

    Initial Commit

---
+++ spotify/spotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  spotifyApp.swift
+//  spotify
+//
+//  Created by Levente Botos on 2023. 07. 02..
+//
+
+import SwiftUI
+
+@main
+struct spotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
